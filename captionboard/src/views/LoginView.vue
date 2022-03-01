f22a0e96a3c2ca8820bf967fb7ad33ddc2509beb
Matthew Dowse <dowsema@tcd.ie>
2022-03-01 21:30:18 +0000

    Package Lock

--- captionboard/src/views/LoginView.vue
+++ captionboard/src/views/LoginView.vue
@@ -3,16 +3,25 @@
         <span v-if="loggedIn">
             {{ this.$router.replace({name: "login"}) }}
         </span>
+
         <form @submit.prevent="pressed">
             <div class="login">
-                <input type="email" placeholder="email" v-model="email">
+                <input type="email" placeholder="Email" v-model="email">
             </div>
+
             <div class="password">
-                <input type="password" placeholder="password" v-model="password">
+                <v-row>
+                    <v-col>
+                        cols="4"
+                        sm="6"
+                        md="3"
+                <input type="password" placeholder="Password" v-model="password">
             </div>
             <v-btn type="submit">Login</v-btn>
         </form>
+
         <div class="error" v-if="error">{{error.message}}</div>
+
         <span>Need an account? Click here to <router-link to="/register">Register</router-link></span>
     </div>
 </template>
