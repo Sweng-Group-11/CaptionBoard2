5dfe62b7018cc668400423db777d5a50b622d329
goleccc <golecl@tcd.ie>
2022-03-01 23:30:56 +0000

    cleaned up the login page a little

--- captionboard/src/views/LoginView.vue
+++ captionboard/src/views/LoginView.vue
@@ -1,19 +1,24 @@
 <template>
-    <div>
+    <div style="text-align:center; color: white">
         <span v-if="loggedIn">
             {{ this.$router.replace({name: "login"}) }}
         </span>
         <form @submit.prevent="pressed">
+        <fieldset class="login-form">
+            <span style="font-size:25px; text-justify: 15px; font-family: Georgia">CaptionBoard Log-In</span>
             <div class="login">
-                <input type="email" placeholder="email" v-model="email">
+                <label for="email" style="padding: 0px">Email</label>
+                <input type="email" id="email" v-model="email" onclick="fnChangeBorder">
             </div>
             <div class="password">
-                <input type="password" placeholder="password" v-model="password">
+                <label for="password" style="padding: 0px;">Password</label>
+                <input type="password" v-model="password">
             </div>
-            <v-btn type="submit">Login</v-btn>
+            <v-btn type="submit" class="loginButton">Login</v-btn>
+            <span>Need an account? Click here to <router-link to="/register" style="color: rgb(235, 242, 250)">Register</router-link></span>
+            </fieldset>
         </form>
         <div class="error" v-if="error">{{error.message}}</div>
-        <span>Need an account? Click here to <router-link to="/register">Register</router-link></span>
     </div>
 </template>
 
@@ -48,19 +53,43 @@
 </script>
 
 <style lang="scss" scoped>
+.loginButton {
+    margin-left: 50%;
+    transform: translate(-50%, 0);
+    max-width: 300px;
+    width: 80%;
+    padding: 15px
+}
+.login-form {
+    background-color: rgb(103, 148, 54);
+    padding-left: 40px;
+    padding-right: 40px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    max-width: 350px;
+    width: 40%;
+    margin-left: 50%;
+    transform: translate(-50%, 0);
+    border-radius: 10px;
+}
+
 input {
-    --topBottomMargin: 20px;
-    max-width: 400px;
-    width: 30%;
+    --topBottomMargin: 15px;
+    max-width: 300px;
+    width: 100%;
     padding: 10px;
     margin-top: var(--topBottomMargin);
     margin-bottom: var(--topBottomMargin);
     margin-left: 50%;
     font-size: 21px;
     text-align: center;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, 0);
+    border-style: solid;
+    border-radius: 5px;
+    border-color: rgb(201, 201, 201);
+    outline-color: rgb(86, 132, 175);
+    background-color: rgb(255, 255, 255);
 }
-
 .error {
     color: red;
 }
