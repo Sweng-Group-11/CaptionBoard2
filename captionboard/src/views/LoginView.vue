65c586d95642032f3f0d588a49a6e16ff2b94264
Matthew Dowse <dowsema@tcd.ie>
2022-03-02 21:46:14 +0000

    Updated Username and Password

--- captionboard/src/views/LoginView.vue
+++ captionboard/src/views/LoginView.vue
@@ -1,76 +1,96 @@
 <template>
-    <div>
-        <span v-if="loggedIn">
-            {{ this.$router.replace({name: "login"}) }}
-        </span>
+  <div>
+    <span v-if="loggedIn">
+      {{ this.$router.replace({ name: "login" }) }}
+    </span>
 
-        <form @submit.prevent="pressed">
-            <div class="login">
-                <input type="email" placeholder="Email" v-model="email">
-            </div>
+    <v-form @submit.prevent="pressed">
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              type="email"
+              label="Email"
+              v-model="email"
+            ></v-text-field>
 
-            <div class="password">
-                <v-row>
-                    <v-col>
-                        cols="4"
-                        sm="6"
-                        md="3"
-                <input type="password" placeholder="Password" v-model="password">
-            </div>
-            <v-btn type="submit">Login</v-btn>
-        </form>
+            <v-text-field
+              v-model="password"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="show1 ? 'text' : 'password'"
+              name="input-10-1"
+              label="Password"
+              hint="At least 8 characters"
+              @click:append="show1 = !show1"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
 
-        <div class="error" v-if="error">{{error.message}}</div>
+      <v-btn type="submit">Login</v-btn>
+    </v-form>
 
-        <span>Need an account? Click here to <router-link to="/register">Register</router-link></span>
-    </div>
+    <div class="error" v-if="error">{{ error.message }}</div>
+
+    <span
+      >Need an account? Click here to
+      <router-link to="/register">Register</router-link></span
+    >
+  </div>
 </template>
 
 <script>
-    import firebase from "firebase/compat/app";
-    import "firebase/compat/auth";
-    export default {
-        data () {
-            return {
-                email: '',
-                password: '',
-                error: ''
-            }
-        },
-        methods: {
-            async pressed() {
-                try {
-                    const val = await firebase.auth().signInWithEmailAndPassword(this.email, this.password);
-                    console.log(val)
-                    await this.$router.replace({name: "dashboard"})
-                } catch(err) {
-                    console.log(err)
-                }
-            },
-            created() {
-            firebase.auth().onAuthStateChanged(user=> {
-                this.loggedIn = !!user;
-            })
-        },
-        }
-    }
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+export default {
+  data() {
+    return {
+      show1: false,
+      email: "",
+      password: "",
+      error: "",
+      rules: {
+        required: (value) => !!value || "Required.",
+        min: (v) => v.length >= 8 || "Min 8 characters",
+        emailMatch: () => `The email and password you entered don't match`,
+      },
+    };
+  },
+  methods: {
+    async pressed() {
+      try {
+        const val = await firebase
+          .auth()
+          .signInWithEmailAndPassword(this.email, this.password);
+        console.log(val);
+        await this.$router.replace({ name: "dashboard" });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    created() {
+      firebase.auth().onAuthStateChanged((user) => {
+        this.loggedIn = !!user;
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 input {
-    max-width: 300px;
-    width: 30%;
-    padding: 10px;
-    margin-left: 50%;
-    transform: translate(-50%, -50%);
-    margin-top: 20px;
-    text-align: center;
-    font-size: 21px;
+  max-width: 300px;
+  width: 30%;
+  padding: 10px;
+  margin-left: 50%;
+  transform: translate(-50%, -50%);
+  margin-top: 20px;
+  text-align: center;
+  font-size: 21px;
 }
 
 .error {
-    color: red;
+  color: red;
 }
-
 </style>
