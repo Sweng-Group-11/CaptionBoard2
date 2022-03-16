0fc15a0124af16c684ad75692d3d8126d9df74cd
Cian Doyle <73841483+ciandoyle251@users.noreply.github.com>
2022-03-16 17:48:19 +0000

    added button for choosing and uploading images

--- captionboard/src/views/AddNewStoryboard.vue
+++ captionboard/src/views/AddNewStoryboard.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
         <span>This is the add new storyboards page</span>
+        <v-btn raised class="primary" @click="onPickFile">upload image</v-btn>
+        <input type="file" style="display:none" ref="fileInput" accept="image/*">
     </div>
+
 </template>
 
 
 <script>
-
-
-
+    export default {
+        methods:    {
+            onPickFile(){
+                this.$refs.fileInput.click();
+            }
+        }
+    }
 </script>
 
 
