3676dbf211af9a9c7f30939de67e4227aafb28c2
Matthew Dowse <dowsema@tcd.ie>
2022-03-24 18:21:22 +0000

    Update AccountSettings.vue

--- captionboard/src/views/AccountSettings.vue
+++ captionboard/src/views/AccountSettings.vue
@@ -1,19 +1,41 @@
+
 <template>
-    <div>
-        <span>This is the account settings page</span>
-    </div>
+  <div id="user">
+    <p
+    >Username: {{ username }}
+    </p>
+
+    <p>
+        Email: 
+    </p>
+
+    <p> 
+        Usertype:
+    </p>    
+
+  </div>
+ <!-- Same Process as above ^ just need Usertype to display if 
+        user is an admin or freelancer, -->
+
 </template>
 
 
+
 <script>
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-
-
+export default {
+  data() {
+    return {
+      username: firebase.auth().currentUser.displayName,
+      uid: firebase.auth().currentUser.uid,
+    };
+  },
+};
 </script>
 
 
-
-
-<style scoped>
-
+<style lang="scss" scoped>
 </style>
