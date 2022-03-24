9ce0d1b2764fe85950885b4f58bef1be7c960b3b
Matthew Dowse <dowsema@tcd.ie>
2022-03-24 18:21:34 +0000

    Update CaptionStoryboards.vue

--- captionboard/src/views/CaptionStoryboards.vue
+++ captionboard/src/views/CaptionStoryboards.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
-        <span>This is the add new captions to storyboards page</span>
-    </div>
+  <v-form>
+    <v-container>
+      <v-col
+          cols="12"
+          sm="6"
+          md="3"
+        >
+          <v-text-field
+            label="Add Caption"
+          ></v-text-field>
+        </v-col>
+
+    </v-container>
+  </v-form>  
 </template>
 
-
 <script>
-
 
 
 </script>
