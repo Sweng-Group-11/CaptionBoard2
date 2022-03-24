236d59d556a87ddea109e566142f79feea925d4c
Matthew Dowse <dowsema@tcd.ie>
2022-03-24 19:11:47 +0000

    Update CaptionStoryboards.vue

--- captionboard/src/views/CaptionStoryboards.vue
+++ captionboard/src/views/CaptionStoryboards.vue
@@ -1,38 +1,22 @@
 <template>
+  <v-expansion-panels accordion>
+    <v-expansion-panel v-for="(item, i) in 5" :key="i">
+      <v-expansion-panel-header>Storyboard Name</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Company names Insert Description Thumbnail image
 
-
-  
-    <v-expansion-panels accordion>
-      <v-expansion-panel
-        v-for="(item,i) in 5"
-        :key="i"
-      >
-        <v-expansion-panel-header>Storyboard Name</v-expansion-panel-header>
-        <v-expansion-panel-content>
-          Company names
-          Insert Description
-          Thumbnail image
-
-          <v-btn elevation="2">
-            Caption this Storyboard
-            </v-btn>
-
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-
-
-
+        <v-btn elevation="2"> Go to Caption this Storyboard </v-btn>
+        
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
-
-
 </script>
 
 
 
 
 <style scoped>
-
 </style>
