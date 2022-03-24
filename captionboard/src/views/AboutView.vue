569cd71a56da052d20b0398ebcc8c0a981659a93
Matthew Dowse <dowsema@tcd.ie>
2022-03-24 18:21:18 +0000

    Update AboutView.vue

--- captionboard/src/views/AboutView.vue
+++ captionboard/src/views/AboutView.vue
@@ -1,38 +1,13 @@
-// I've been using this page to test various things, don't worry about deleting any of it
-// because none of it is important. -Wes
 
 <template>
   <div class="about">
-    This is an about page.
+    ADAPT
   </div>
+
 </template>
 
+
+
 <script>
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
 
-export default {
-  methods: {
-    async getUserType() {
-      firebase
-        .firestore()
-        .collection("users")
-        .doc(firebase.auth().currentUser.uid)
-        .get()
-        .then((ds) => {
-          this.user_type = ds.get("test2");
-          console.log("Test:", ds.data());
-          console.log("User Type: ", this.user_type);
-          console.log("ds: ", ds);
-        });
-    },
-  },
-
-  data() {
-    return {
-      user_type: "no",
-    };
-  },
-};
 </script>
