a902643bae513a93c5c3d7d162d9ea66fee3e538
Matthew Dowse <dowsema@tcd.ie>
2022-03-24 19:11:45 +0000

    Create FreelancerViewStoryboard.vue

---
+++ captionboard/src/views/FreelancerViewStoryboard.vue
@@ -0,0 +1,30 @@
+<!-- Page to use to display specifickly clicked storyboard on freelancers side -->
+
+
+<template>
+    <v-form>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="3">
+                <v-text-field label="Regular"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+
+        
+</template>
+
+
+<script>
+
+
+
+</script>
+
+
+
+
+<style scoped>
+
+</style>        
