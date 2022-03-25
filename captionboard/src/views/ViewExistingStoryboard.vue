c54871314aabcc12d300eaa34f6d16c7d355a309
Matthew Dowse <dowsema@tcd.ie>
2022-03-25 13:28:21 +0000

    Update ViewExistingStoryboard.vue

--- captionboard/src/views/ViewExistingStoryboard.vue
+++ captionboard/src/views/ViewExistingStoryboard.vue
@@ -1,39 +1,50 @@
 <template>
   <div>
+    <v-expansion-panels accordion>
+      <v-expansion-panel v-for="(item, i) in 5" :key="i">
+        <v-expansion-panel-header> Storyboard Name </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <div class="blue--text">Description:</div>
+
+          <v-card max-width="344">
+            <!-- Thumbnail Image-->
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-actions>
+              <div class="blue--text">Captions for this Image</div>
+
+              <v-spacer></v-spacer>
+
+              <v-btn icon @click="show = !show">
+                <v-icon>{{
+                  show ? "mdi-chevron-up" : "mdi-chevron-down"
+                }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                  This will be a list of all captions for this image
+                </v-card-text>
+              </div>
+            </v-expand-transition>
+          </v-card>
+
+          <!-- Insert Images -->
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
     <p></p>
     <img id="dog" src="test" height="125px" width="200px" />
     <h1>This image has been downloaded from the database.</h1>
     <p>(Proof of concept)</p>
-    <v-card class="mx-auto" max-width="344">
-      <!-- Thumbnail Image-->
-      <v-img
-        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-        height="200px"
-      ></v-img>
-
-      <v-card-title> Name of Storyboard </v-card-title>
-
-      <v-card-subtitle> Company Name </v-card-subtitle>
-
-      <v-card-actions>
-        <v-btn color="blue lighten-2" text> View Storyboard </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-btn icon @click="show = !show">
-          <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
-        </v-btn>
-      </v-card-actions>
-
-      <v-expand-transition>
-        <div v-show="show">
-          <v-divider></v-divider>
-
-          <!--Quick Description -->
-          <v-card-text> Quick Description of Storyboard </v-card-text>
-        </div>
-      </v-expand-transition>
-    </v-card>
   </div>
 </template>
 
@@ -62,9 +73,9 @@ export default {
     this.showImage();
   },
 
-  data: () => {
-    return {};
-  },
+  data: () => ({
+    show: false,
+  }),
 };
 </script>
 
