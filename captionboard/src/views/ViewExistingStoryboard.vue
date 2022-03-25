664eef7add3f8cb4d3c6a5a393c9d2121fd4a8c2
WesLemon <wesleyshaw25@gmail.com>
2022-03-25 15:27:10 +0000

    successfully displayed storyboard from backend

--- captionboard/src/views/ViewExistingStoryboard.vue
+++ captionboard/src/views/ViewExistingStoryboard.vue
@@ -1,21 +1,13 @@
 <template>
     <div>
-
-        <div>
-        <p> </p>
-        <img id="dog" src="test" height="125px" width="200px"/>
-        <h1>This image has been downloaded from the database.</h1>
-        <p>(Proof of concept)</p>    
-        </div>
       
-        <!-- <div id="photo-grid">
-          <photo-grid
-            :box-height="'400px'"
-            :box-width="'30%'"
-            <img v-for="image in "
-          >
-          </photo-grid>
-      </div> -->
+        <div v-for="(image, imageIndex) in imageURLs " :key="imageIndex">
+          <img 
+            :src="image"
+            width="20%"
+            height=auto
+          />        
+        </div>
 
     </div>
 </template>
@@ -23,51 +15,27 @@
 <script>
 import firebase from "firebase/compat/app";
 import "firebase/compat/storage";
-import PhotoGrid from 'vue-photo-grid'
 
-Vue.use(PhotoGrid);
 
 export default {  
 
   methods: {
-    // async showImage() {
-    //     const storageRef = firebase.storage().ref();
-    //     storageRef.child('testing/dog.jpg').getDownloadURL().then(function(url) {
-    //         const img = document.getElementById('dog');
-    //         img.src = url;
-    //     }).catch(function(error) {
-    //         console.log(error)
-    //     });
-    // },
-
     async showStoryboard() {
 
+      const imageURLs = [];
 
       const storageRef = firebase.storage().ref("storyboards/user1/storyboard1");
       storageRef.listAll().then(function(result) {
         result.items.forEach(function(image) {
           image.getDownloadURL().then(function(url) {
-            this.imageURLs.push(url)
+            imageURLs.push(url)
           })
         });
       }).catch(function(error) {
         console.log(error)
+      }).then(() => {
+        this.imageURLs = imageURLs;
       });
-
-      this.imageURLs.forEach(function(image) {
-        var img = document.createElement('img');
-        img.src = image;
-        document.body.appendChild(img);
-      });
-      
-      // function displayImage(image) {
-      //   image.getDownloadURL().then(function(url) {
-      //     const img = document.getElementById('dog');
-      //     img.src = url;
-      //   }).catch(function(error) {
-      //     console.log(error)
-      //   });
-      // }
     }
   },
   
