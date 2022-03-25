0055e34d4cbe9ba9444b0fd3d2a9f58c91f29db1
Matthew Dowse <dowsema@tcd.ie>
2022-03-25 10:55:53 +0000

    Update HomeView.vue

--- captionboard/src/views/HomeView.vue
+++ captionboard/src/views/HomeView.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+<div
+    style="text-align: center; color: black; font-family: 'Ubuntu', sans-serif;"
+  >
+  <div class = "hello">
+    <div class ="name">
+      <IonPage>
+
+    <ion-content :fullscreen="true" id="start">
+
+      <div class = "letter">
+        <img src ="../assets/captionboard_logo_words_colour_outline copy.png">
+        <h1>Welcome to CaptionBoard, Login to continue</h1>
+          
+      </div>
+</ion-content>
+
+  </IonPage>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  name: 'Home-page',
+  components:{}
 };
 </script>
