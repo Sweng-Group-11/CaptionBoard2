a6fcf0b1c3899498bde65b655eaccebfb763fe24
WesLemon <wesleyshaw25@gmail.com>
2022-03-30 14:50:32 +0100

    captions not going to database yet

--- captionboard/src/views/CaptionStoryboards.vue
+++ captionboard/src/views/CaptionStoryboards.vue
@@ -4,6 +4,7 @@
       <v-expansion-panel
         v-for="(name, nameIndex) in storyboardNames"
         :key="nameIndex"
+        v-model="storyboard_name"
       >
         <v-expansion-panel-header>{{ name }}</v-expansion-panel-header>
         <v-expansion-panel-content>
@@ -13,22 +14,26 @@
             v-for="(image, imageIndex) in imageRefs[nameIndex]"
             :key="imageIndex"
           >
-            <v-card class="mx-auto" max-width="30%">
+            <v-card class="mx-auto" max-width="250">
               <v-img :src="image" height="auto"></v-img>
-
+              <p></p>
               <v-card-actions>
                 <div class="blue--text">
-                  <v-form>
-                    <v-container>
-                      <v-row>
-                        <v-col>
-                          <v-text-field
-                            label="Insert caption here"
-                          ></v-text-field>
-                        </v-col>
-                      </v-row>
-                    </v-container>
-                  </v-form>
+                  <form>
+                    <v-text-field
+                      v-model="caption"
+                      :error-messages="captionErrors"
+                      :counter="100"
+                      label="Enter caption here"
+                      required
+                      @input="$v.caption.$touch()"
+                      @blur="$v.caption.$touch()"
+                    ></v-text-field>
+
+                    <v-btn class="mr-4" @click="submit"> submit </v-btn>
+                    <v-btn @click="clear"> clear </v-btn>
+                  </form>
+                  <p>{{ test }}</p>
                 </div>
                 <v-spacer></v-spacer>
               </v-card-actions>
@@ -45,9 +50,72 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/storage";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import { validationMixin } from "vuelidate";
+import { required, maxLength } from "vuelidate/lib/validators";
 
 export default {
+  mixins: [validationMixin],
+
+  validations: {
+    caption: { required, maxLength: maxLength(100) },
+  },
+
+  computed: {
+    captionErrors() {
+      const errors = [];
+      if (!this.$v.caption.$dirty) return errors;
+      !this.$v.caption.maxLength &&
+        errors.push("caption must be at most 100 characters long");
+      !this.$v.caption.required && errors.push("caption is required.");
+      return errors;
+    },
+  },
+
   methods: {
+    submit() {
+      this.$v.$touch();
+      if (this.caption != "") {
+        const caption = this.caption;
+        const name = this.storyboard_name;
+
+        alert(name);
+        this.test = this.caption;
+        const numRef = firebase
+        .firestore()
+        .collection("users")
+        .doc("testID")
+        .collection("captions")
+        .doc("num_captions");
+
+      numRef.get().then(function (number) {
+        let num_captions = number.get("num");
+        num_captions++;
+        numRef.set({
+          num: num_captions,
+        });
+
+        const num = num_captions.toString();
+
+        firebase
+          .firestore()
+          .collection("users")
+          .doc("testID")
+          .collection("captions")
+          .doc(num)
+          .set({
+            caption: caption,
+            admin_id: "testID",
+
+          });
+      });
+      }
+
+      
+    },
+    clear() {
+      this.$v.$reset();
+      this.caption = "";
+    },
     async populateStoryboards() {
       const storyboardRefs = firebase.firestore().collection("storyboards");
 
@@ -114,6 +182,9 @@ export default {
       storyboardNames: [],
       imageRefs: [],
       storyboardDescs: [],
+      caption: "",
+      test: "empty",
+      storyboard_name: "",
     };
   },
 };
